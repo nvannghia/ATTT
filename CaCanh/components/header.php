e3bc103394f988435e0ea154536e22e6aa4bb0ae
phuongnam <2051052085nam@ou.edu.vn>
2023-03-09 11:57:54 +0700

    navbar

--- CaCanh/components/header.php
+++ CaCanh/components/header.php
@@ -13,3 +13,26 @@
     <title>Cuahangcacanh.vn</title>
 </head>
 <body>
+    <!-- Phần của Phương Nam -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="javascript:void(0)">Trang chủ</a>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:void(0)">Giỏ hàng</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:void(0)">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:void(0)">Link</a>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    </nav>
+    <!--  -->
+
+    
+</body>
