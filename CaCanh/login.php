a2a1e996065c0e8b207f0d68007be472ed66abb0
nghia nguyen <nghiaturtle2002@gmail.com>
2023-03-10 23:18:09 +0700

    update

--- CaCanh/login.php
+++ CaCanh/login.php
@@ -1,5 +1,4 @@
 <?php
-require 'configuration/database.php';
 include "ceasar.php";
 session_start();
 if (isset($_POST['login'])) {
@@ -36,7 +35,7 @@ if (isset($_POST['login'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="style.css">
   <title>Đăng nhập</title>
 </head>
 
