b7e6da7b27b232ea86ad46bb7ed1890271d16a3d
nghia nguyen <nghiaturtle2002@gmail.com>
2023-03-13 22:02:59 +0700

    update interface

--- CaCanh/index.php
+++ CaCanh/index.php
@@ -18,14 +18,14 @@
                 $price = number_format( $fishinfo['price']);
                 echo "
                 <div class='col-md-3 margin-bottom'> 
-                 <div class='card' style='width: 18rem;'>
-                     <img class='card-img-top image-top' style='width:287px;height:200px'  src='Images/$imageurl' alt='$imageurl'>
+                 <div class='card' style='width: 18rem; box-shadow:5px 5px 0 rgb(234, 228, 228);'>
+                     <img class='card-img-top image-top' style='width:287px;height:200px;'  src='Images/$imageurl' alt='$imageurl'>
                      <div class='card-body'>
                      <h5 class='card-title'>$fish_name</h5>
                      <p class='card-text'>$price vnđ</p>
                      <form action='carts.php' method='post' style='border:none'> 
                         <input type='number' name ='quantity' min='1' max='50' value='1'>
-                        <input type='submit' name ='addtocart' class='btn btn-primary' value ='Thêm vào giỏ hàng'>
+                        <input type='submit' name ='addtocart' class='button btn btn-primary' value ='Thêm vào giỏ hàng'>
                         <input type='hidden' name='fish_name' value='$fish_name'>
                         <input type='hidden' name='price_beforeFortmat' value='$price_beforeFortmat'>
                         <input type='hidden' name='image' value='Images/$imageurl'>
--- CaCanh/searchcart.php
+++ CaCanh/searchcart.php
@@ -3,7 +3,7 @@ require 'components/header.php';
 if(isset($_POST['searchs']) && ($_POST['searchs']))
 {
    $search=$_POST['search']; 
-$sql = "SELECT * FROM attt.Fish where LOWER(name) LIKE '%$search%'";
+   $sql = "SELECT * FROM attt.Fish where LOWER(name) LIKE '%$search%'";
 if($connection != null)
 {
     echo'<div class="container">';
@@ -13,6 +13,8 @@ if($connection != null)
         $stm->execute();
         $result = $stm->setFetchMode(PDO::FETCH_ASSOC);
         $fishInfos = $stm->fetchALL();
+        if(empty($fishInfos))
+         echo "<h1 style='color:white;'>Không tìm thấy kết quả của : ".$search."</h1>";
         echo '<div class="row">';
         foreach($fishInfos as $fishinfo)
         {
@@ -22,14 +24,14 @@ if($connection != null)
             $price = number_format( $fishinfo['price']);
             echo "
             <div class='col-md-3 margin-bottom'> 
-             <div class='card' style='width: 18rem;'>
-                 <img class='card-img-top image-top' style='width:287px;height:200px'  src='Images/$imageurl' alt='$imageurl'>
+            <div class='card' style='width: 18rem; box-shadow:5px 5px 0 rgb(234, 228, 228);'>
+                 <img class='card-img-top image-top' style='width:287px;height:200px;'  src='Images/$imageurl' alt='$imageurl'>
                  <div class='card-body'>
                  <h5 class='card-title'>$fish_name</h5>
                  <p class='card-text'>$price vnđ</p>
                  <form action='carts.php' method='post' style='border:none'> 
                     <input type='number' name ='quantity' min='1' max='50' value='1'>
-                    <input type='submit' name ='addtocart' class='btn btn-primary' value ='Thêm vào giỏ hàng'>
+                    <input type='submit' name ='addtocart' class='btn btn-primary button' value ='Thêm vào giỏ hàng'>
                     <input type='hidden' name='fish_name' value='$fish_name'>
                     <input type='hidden' name='price_beforeFortmat' value='$price_beforeFortmat'>
                     <input type='hidden' name='image' value='Images/$imageurl'>
